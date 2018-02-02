7220d33a39ba25938df7183801718cc464495e2c
UselessPickles <jlau@uselesspickles.com>
2018-02-02 01:08:51 -0500

    Upgrade to TypeScript 2.7.1 and uncomment a previously failing compile test.

--- tests/compile_samples/visitString/pass/enum.enum-props.inferred-param-type.ts
+++ tests/compile_samples/visitString/pass/enum.enum-props.inferred-param-type.ts
@@ -1,26 +1,22 @@
-// TEST DISABLED!
-// See https://github.com/UselessPickles/ts-string-visitor/issues/3
-// Uncomment the code after TypeScript is updated to fix the bug.
+import { visitString } from "../../../../src";
 
-// import { visitString } from "../../../../src";
+enum RGB {
+    R = "r",
+    G = "g",
+    B = "b"
+}
 
-// enum RGB {
-//     R = "r",
-//     G = "g",
-//     B = "b"
-// }
-
-// visitString("r" as RGB).with({
-//     [RGB.R]: (value) => {
-//         // tslint:disable-next-line:no-unused-variable
-//         const test: RGB.R = value;
-//     },
-//     [RGB.G]: (value) => {
-//         // tslint:disable-next-line:no-unused-variable
-//         const test: RGB.G = value;
-//     },
-//     [RGB.B]: (value) => {
-//         // tslint:disable-next-line:no-unused-variable
-//         const test: RGB.B = value;
-//     }
-// });
+visitString("r" as RGB).with({
+    [RGB.R]: (value) => {
+        // tslint:disable-next-line:no-unused-variable
+        const test: RGB.R = value;
+    },
+    [RGB.G]: (value) => {
+        // tslint:disable-next-line:no-unused-variable
+        const test: RGB.G = value;
+    },
+    [RGB.B]: (value) => {
+        // tslint:disable-next-line:no-unused-variable
+        const test: RGB.B = value;
+    }
+});
