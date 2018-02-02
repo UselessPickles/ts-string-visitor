1b36a5bf6976062fd5340367ce3231ef4a824b94
UselessPickles <jlau@uselesspickles.com>
2018-02-02 00:35:40 -0500

    Add mapString() compile pass/fail tests for "handleUnexpected"

---
+++ tests/compile_samples/mapString/pass/literal-null.explicit-return-type.unexpected.ts
@@ -0,0 +1,14 @@
+import { mapString } from "../../../../src";
+
+type RGB = "r" | "g" | "b";
+
+const result = mapString("r" as RGB | null).with<number>({
+    "r": 1,
+    "g": 2,
+    "b": 3,
+    handleNull: 0,
+    handleUnexpected: -1
+});
+
+// tslint:disable-next-line:no-unused-variable
+const test: number = result;
