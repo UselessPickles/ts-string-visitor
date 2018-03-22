08fdae9187c2d4f748e01ec699f7983eff172026
UselessPickles <jlau@uselesspickles.com>
2018-03-21 23:54:30 -0400

    Add dtslint tests for mapString

---
+++ type_tests/v2_4_plus/mapString.enum-both.dtslint.ts
@@ -0,0 +1,75 @@
+import { mapString } from "../../src";
+
+enum RGB {
+    R = "r",
+    G = "g",
+    B = "b"
+}
+
+declare const rgb: RGB | null | undefined;
+
+// Return type is inferred
+// $ExpectType number
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleNull: -1,
+    handleUndefined: -1
+});
+// $ExpectType string
+mapString(rgb).with({
+    r: "10",
+    g: "20",
+    b: "30",
+    handleNull: "-1",
+    handleUndefined: "-1"
+});
+
+// handleUnexpected is allowed
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleNull: -1,
+    handleUndefined: -1,
+    handleUnexpected: -1
+});
+
+// Missing value handler causes error
+// $ExpectError
+mapString(rgb).with({
+    r: 10,
+    b: 30,
+    handleNull: -1,
+    handleUndefined: -1
+});
+
+// Unexpected value handler causes error
+mapString(rgb).with({
+    r: 10,
+    // $ExpectError
+    oops: 42,
+    g: 20,
+    b: 30,
+    handleNull: -1,
+    handleUndefined: -1
+});
+
+// missing null handler causes error
+// $ExpectError
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleUndefined: -1
+});
+
+// missing undefined handler causes error
+// $ExpectError
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleNull: -1
+});
---
+++ type_tests/v2_4_plus/mapString.enum-undefined.dtslint.ts
@@ -0,0 +1,70 @@
+import { mapString } from "../../src";
+
+enum RGB {
+    R = "r",
+    G = "g",
+    B = "b"
+}
+
+declare const rgb: RGB | undefined;
+
+// Return type is inferred
+// $ExpectType number
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleUndefined: -1
+});
+// $ExpectType string
+mapString(rgb).with({
+    r: "10",
+    g: "20",
+    b: "30",
+    handleUndefined: "-1"
+});
+
+// handleUnexpected is allowed
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    handleUndefined: -1,
+    handleUnexpected: -1
+});
+
+// Missing value handler causes error
+// $ExpectError
+mapString(rgb).with({
+    r: 10,
+    b: 30,
+    handleUndefined: -1
+});
+
+// Unexpected value handler causes error
+mapString(rgb).with({
+    r: 10,
+    // $ExpectError
+    oops: 42,
+    g: 20,
+    b: 30,
+    handleUndefined: -1
+});
+
+// missing undefined handler causes error
+// $ExpectError
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30
+});
+
+// Unnecessary null handler causes error
+mapString(rgb).with({
+    r: 10,
+    g: 20,
+    b: 30,
+    // $ExpectError
+    handleNull: -1,
+    handleUndefined: -1
+});
