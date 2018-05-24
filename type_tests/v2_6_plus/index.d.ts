be9b5621166a03bc51e48b3c4c73b4b431ef92b7
UselessPickles <jlau@uselesspickles.com>
2018-05-23 22:39:24 -0400

    Disable rule in type tests via tslint.json instead of comment

--- type_tests/v2_6_plus/index.d.ts
+++ type_tests/v2_6_plus/index.d.ts
@@ -1,4 +1,3 @@
-// tslint:disable:no-useless-files
 // Dummy index.d.ts file for running dtslint.
 // We're not actually linting a .d.ts file; only using dtslint to verify compile time types of ts-string-visitor.
 // TypeScript Version: 2.6
